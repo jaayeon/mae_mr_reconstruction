fix normalization

engine_pretrain.py
@@ -10,7 +10,9 @@
 # --------------------------------------------------------
 import math
 import sys
+import os
 from typing import Iterable
+import imageio
 
 import torch
 
@@ -42,8 +44,8 @@ def train_one_epoch(model: torch.nn.Module,
     for data_iter_step, data in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
 
         # we use a per iteration (instead of per epoch) lr scheduler
-        if data_iter_step % accum_iter == 0:
-            lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+        # if data_iter_step % accum_iter == 0:
+        #     lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
 
         samples = data['down'].to(device, non_blocking=True)
         ssl_masks = data['mask'].to(device, non_blocking=True)
@@ -62,6 +64,9 @@ def train_one_epoch(model: torch.nn.Module,
         loss /= accum_iter
         loss_scaler(loss, optimizer, parameters=model.parameters(),
                     update_grad=(data_iter_step + 1) % accum_iter == 0)
+        # loss.backward()
+        # optimizer.step()
+
         if (data_iter_step + 1) % accum_iter == 0:
             optimizer.zero_grad()
 
@@ -80,8 +85,14 @@ def train_one_epoch(model: torch.nn.Module,
             epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
             log_writer.add_scalar('train_loss', loss_value_reduce, epoch_1000x)
             log_writer.add_scalar('lr', lr, epoch_1000x)
-
-
+        # i=0
+        # for name, param in model.named_parameters():
+        #     i+=1
+        #     if i==9 and param.requires_grad:
+        #         print(name)
+        #         print('during model.params: \n', param.data)
+        #         print('during model.params.grad: \n', param.grad)
+        #         break
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
@@ -99,18 +110,41 @@ def valid_one_epoch(model: torch.nn.Module,
     valid_stats = {k:0 for k in keys}
     vnum = len(data_loader)
 
+    save_folder = os.path.join(args.output_dir, 'valid_epoch{}'.format(epoch))
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
+
     with torch.no_grad():
-        for data in data_loader:
+        for i, data in enumerate(data_loader):
             samples = data['down'].to(device, non_blocking=True)
-            ssl_masks = data['mask'].to(device, non_blocking=True)
+            ssl_masks = data['mask'].to(device, non_blocking=True) # 0 is keep, 1 is remove
             full_samples = data['full'].to(device, non_blocking=True)
-            loss, pred, _  = model(samples, ssl_masks)
+            _, pred, _  = model(samples, ssl_masks)
+
+            # Data consistency
+            pred = torch.clamp(pred, min=-1, max=1)
+            pred = samples + pred*ssl_masks
             
             samples, pred, full = rifft2(samples[0,:,:,:], pred[0,:,:,:], full_samples[0,:,:,:], permute=True) 
             
+            #normalization [0-1]
+            max = torch.max(samples)
+            min = torch.min(samples)
+            samples = torch.clamp((samples-min)/(max-min), min=0, max=1)
+            pred = torch.clamp((pred-min)/(max-min), min=0, max=1)
+            full = torch.clamp((full-min)/(max-min), min=0, max=1)
+            
+            #calculate psnr, ssim
             stat = calc_metrics(samples.unsqueeze(0), pred.unsqueeze(0), full.unsqueeze(0))
             for k,v in stat.items():
                 valid_stats[k]+=v/vnum
+            
+            #image save
+            if i%50==0:
+                imageio.imwrite(os.path.join(save_folder, 'down_{:03d}.tif'.format(int(i/10))), samples.squeeze().cpu().numpy())
+                imageio.imwrite(os.path.join(save_folder, 'pred_{:03d}.tif'.format(int(i/10))), pred.squeeze().cpu().numpy())
+                imageio.imwrite(os.path.join(save_folder, 'full_{:03d}.tif'.format(int(i/10))), full.squeeze().cpu().numpy())
+                imageio.imwrite(os.path.join(save_folder, 'concat_{:03d}.tif'.format(int(i/10))), torch.cat([samples, pred, full], dim=-1).cpu().numpy())
 
     print('Validation Epoch: {} {}'.format(epoch, ', '.join(['{}: {:.3f}'.format(k,v.item()) for k,v in valid_stats.items()])))
     return valid_stats

main_pretrain.py
@@ -69,7 +69,8 @@ def get_args_parser():
     parser.set_defaults(norm_pix_loss=False)
     parser.add_argument('--ssl_loss', action='store_true',
                         help='make two different augmentation for each data, and calculate self supervised loss')
-
+    parser.add_argument('--ssl_weight', type=float, default=1, help='weight of ssl loss related to sp_loss')
+    
     # Optimizer parameters
     parser.add_argument('--weight_decay', type=float, default=0.05,
                         help='weight decay (default: 0.05)')
@@ -90,6 +91,7 @@ def get_args_parser():
     parser.add_argument('--down_factor', type=int, default=2, help='downsampling factor of original data')
     parser.add_argument('--low_freq_ratio', type=float, default=0.7, help='ratio of low frequency lines in undersampled data')
     parser.add_argument('--no_center_mask', action='store_true', help='preserving center in kspace from random_masking')
+
     # Dataset parameters
     parser.add_argument('--data_path', default='../../data/', type=str,
                         help='dataset path')
@@ -160,7 +162,7 @@ def main(args):
     '''
     dataset = IXIDataset(args, mode='train')
 
-    tnum = int(len(dataset)*0.9)
+    tnum = int(len(dataset)*0.95)
     vnum = len(dataset)-tnum
     dataset_train, dataset_valid = torch.utils.data.random_split(dataset, [tnum, vnum])
 
@@ -197,7 +199,8 @@ def main(args):
         drop_last=False
     )
     # define the model
-    model = models_mae.__dict__[args.model](norm_pix_loss=args.norm_pix_loss, ssl_loss=args.ssl_loss, no_center_mask=args.no_center_mask)
+    model = models_mae.__dict__[args.model](norm_pix_loss=args.norm_pix_loss, ssl_loss=args.ssl_loss, 
+                                            ssl_weight=args.ssl_weight, no_center_mask=args.no_center_mask)
 
     model.to(device)
 
@@ -237,12 +240,28 @@ def main(args):
         # train
         if args.distributed:
             data_loader_train.sampler.set_epoch(epoch)
+        # i=0
+        # for name, param in model.named_parameters():
+        #     i+=1
+        #     if i==9 and param.requires_grad:
+        #         print(name)
+        #         print('before model.params: \n', param.data)
+        #         print('before model.params.grad: \n', param.grad)
+        #         break
         train_stats = train_one_epoch(
             model, data_loader_train,
             optimizer, device, epoch, loss_scaler,
             log_writer=log_writer,
             args=args
         )
+        # i=0
+        # for name, param in model.named_parameters():
+        #     i+=1
+        #     if i==9 and param.requires_grad:
+        #         print(name)
+        #         print('after model.params: \n', param.data)
+        #         print('after model.params.grad: \n', param.grad)
+        #         break
         if args.output_dir and (epoch % 50 == 0 or epoch + 1 == args.epochs):
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
@@ -258,36 +277,14 @@ def main(args):
                 f.write(json.dumps(train_log_stats) + "\n")
         
         # validation
-
         valid_stats = valid_one_epoch(
             model, data_loader_valid, 
             device, epoch, args=args
         )
-
         valid_log_stats = {**{f'valid_{k}': v.item() for k,v in valid_stats.items()}, 'epoch':epoch,}
-        
-        """ model.train=False
-        keys=['noise_loss', 'loss', 'noise_psnr', 'psnr', 'noise_ssim', 'ssim', 'noise_nmse', 'nmse']
-        valid_log_stats = {k:0 for k in keys}
-        vnum = len(data_loader_valid)
-        for data in data_loader_valid:
-            with torch.no_grad():
-                samples = data['down'].to(device, non_blocking=True)
-                ssl_masks = data['mask'].to(device, non_blocking=True)
-                full_samples = data['full'].to(device, non_blocking=True)
-                loss, pred, _  = model(samples, ssl_masks)
-                
-                samples, pred, full = rifft2(samples[0,:,:,:], pred[0,:,:,:], full_samples[0,:,:,:], permute=True) 
-                
-                stat = calc_metrics(samples.unsqueeze(0), pred.unsqueeze(0), full.unsqueeze(0))
-                for k,v in stat.items():
-                    valid_log_stats[k]+=v/vnum
 
-        print('Validation Epoch: {} {}'.format(epoch, ', '.join(['{}: {:.3f}'.format(k,v.item()) for k,v in valid_log_stats.items()])))
-        valid_log_stats = {**{f'valid_{k}': v.item() for k,v in valid_log_stats.items()}, 'epoch':epoch,} 
-        """
         if valid_log_stats['valid_psnr']>best_psnr:
-            'Save Best Checkpoint'
+            print('Save Best Checkpoint')
             best_psnr = valid_log_stats['valid_psnr']
             #save
             misc.save_model(

models_mae.py
@@ -25,7 +25,7 @@ class MaskedAutoencoderViT(nn.Module):
     def __init__(self, img_size=256, patch_size=16, in_chans=1,
                  embed_dim=1024, depth=24, num_heads=16,
                  decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
-                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False, ssl_loss=False, no_center_mask=False):
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False, ssl_loss=False, ssl_weight=1, no_center_mask=False):
         super().__init__()
 
         # --------------------------------------------------------------------------
@@ -60,6 +60,7 @@ class MaskedAutoencoderViT(nn.Module):
 
         self.norm_pix_loss = norm_pix_loss
         self.ssl_loss = ssl_loss
+        self.ssl_weight = ssl_weight
         self.no_center_mask = no_center_mask
         self.train = True
         self.img_size = img_size
@@ -250,6 +251,7 @@ class MaskedAutoencoderViT(nn.Module):
         pred: [N, L, p*p*2]
         mask: [N, L], 0 is keep, 1 is remove, 
         ssl_masks: [N, 1, H, W] 0 is keep, 1 is remove
+        sp_masks: [N, 1, H, W] 0 is remove, 1 is keep
         """
         N,L,_=pred.shape
         if self.ssl_loss:
@@ -268,16 +270,18 @@ class MaskedAutoencoderViT(nn.Module):
             var = target.var(dim=-1, keepdim=True)
             target = (target - mean) / (var + 1.e-6)**.5
 
-        loss = (pred - target) ** 2
+        # loss = (pred - target) ** 2
+        loss = torch.abs(pred - target)
         loss = loss*sp_masks
         loss = loss.mean(dim=-1)  # [N, L], mean loss per patch
 
-        #loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
-        loss = loss.sum() / N # mean loss on every patches
+        # loss = (loss * mask).sum() / N  # mean loss on removed patches
+        loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
+        # loss = loss.sum() / N # mean loss on every patches
 
         if self.ssl_loss:
             sslloss = self.get_ssl_loss(pred, ssl_masks)
-            loss = (loss+sslloss)/2
+            loss = loss+self.ssl_weight*sslloss
         return loss
     
     def get_ssl_loss(self, pred, ssl_masks):
@@ -289,9 +293,10 @@ class MaskedAutoencoderViT(nn.Module):
             pred1 = pred[i,:,:]
             pred2 = pred[i+int(N/2),:,:]
             mask = ssl_masks[i,:,:]
-            loss = (pred1-pred2)**2
+            # loss = (pred1-pred2)**2
+            loss = torch.abs(pred1-pred2)
 
-            loss = (loss*mask).sum()/N*2
+            loss = (loss*mask).sum()/N/L*2
             # loss = loss.sum() #mean loss on every piexles
 
             sslloss+=loss
@@ -300,10 +305,11 @@ class MaskedAutoencoderViT(nn.Module):
     def forward(self, imgs, ssl_masks, mask_ratio=0.75, num_low_freqs=None):
         latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio, num_low_freqs)
         pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
-        loss = self.forward_loss(imgs, pred, mask, ssl_masks) #mask: 0 is keep, 1 is remove
         if self.train:
+            loss = self.forward_loss(imgs, pred, mask, ssl_masks) #mask: 0 is keep, 1 is remove
             return loss, pred, mask
         else: 
+            loss = None
             return loss, self.unpatchify(pred), mask
 
 

util/metric.py
@@ -11,7 +11,7 @@ from torch.autograd import Variable
 def calc_metrics(x, out, target, data_range=1, window_size=11, channel=1, size_average=True):
 
     #normalize
-    x, out, target = normalize(x, out, target)
+    # x, out, target = normalize(x, out, target)
     
     #compute nmse
     noise_nmse = compute_NMSE(x, target)
@@ -35,16 +35,13 @@ def calc_metrics(x, out, target, data_range=1, window_size=11, channel=1, size_a
 
     return results
 
-def normalize(*img, eps=1e-08):
-    def _normalize(img, eps=eps):
-        max = torch.max(img)
-        min = torch.min(img)
-        img = (img-min)/(max-min+eps)
-        return img
-    if len(img)==1:
-        return _normalize(img[0])
-    else:
-        return [_normalize(im) for im in img]
+def normalize(x, out, target, eps=1e-08):
+    max = torch.max(x)
+    min = torch.min(x)
+    x = (x-min)/(max-min+eps)
+    out = torch.clamp((out-min)/(max-min+eps), min=0, max=1)
+    target = torch.clamp((target-min)/(max-min+eps), min=0, max=1)
+    return [x, out, target]
     
 
 def compute_NMSE(pred, gt):