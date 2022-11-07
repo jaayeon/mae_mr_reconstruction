fix supervised learning loss & 1d masking to reflect downsample, mk small model

engine_pretrain.py
@@ -60,17 +60,20 @@ def train_one_epoch(model: torch.nn.Module,
             sploss, sslloss, pred, _ = model(samples, ssl_masks, full_samples, mask_ratio=args.mask_ratio)
 
 
-        """ # spatial domain loss
-        pred, full = rifft2(pred[:,:,:,:], full_samples[:,:,:,:], permute=True) 
+        # spatial domain loss
+        if args.downsample>1:
+            pred_dc = samples + pred*ssl_masks
+        else:
+            pred_dc = pred
+        pred_dc, full = rifft2(pred_dc[:,:,:,:], full_samples[:,:,:,:], permute=True) 
         maxnum = torch.max(full)
         minnum = torch.min(full)
-        pred = (pred-minnum)/(maxnum-minnum+1e-08)
+        pred_dc = (pred_dc-minnum)/(maxnum-minnum+1e-08)
         full = (full-minnum)/(maxnum-minnum+1e-08)
-        sptloss = torch.sum(abs(pred-full))/samples.shape[0]      
-        """
-        sptloss = torch.tensor([0], device=sploss.device)
+        imgloss = torch.sum(torch.abs(pred_dc-full))/samples.shape[0]/256      
+        # imgloss = torch.tensor([0], device=sploss.device)
 
-        loss = sploss + args.ssl_weight*sslloss + sptloss
+        loss = sploss + args.ssl_weight*sslloss + args.img_weight*imgloss
         loss_value = loss.item()
 
         if not math.isfinite(loss_value):
@@ -89,7 +92,7 @@ def train_one_epoch(model: torch.nn.Module,
         metric_logger.update(loss=loss_value)
         metric_logger.update(sploss=sploss.item())
         metric_logger.update(sslloss=sslloss.item())
-        metric_logger.update(spaloss=sptloss.item())
+        metric_logger.update(imgloss=imgloss.item())
 
         lr = optimizer.param_groups[0]["lr"]
         metric_logger.update(lr=lr)
@@ -97,7 +100,7 @@ def train_one_epoch(model: torch.nn.Module,
         loss_value_reduce = misc.all_reduce_mean(loss_value)
         sploss_value_reduce = misc.all_reduce_mean(sploss.item())
         sslloss_value_reduce = misc.all_reduce_mean(sslloss.item())
-        sptloss_value_reduce = misc.all_reduce_mean(sptloss.item())
+        sptloss_value_reduce = misc.all_reduce_mean(imgloss.item())
         if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
             """ We use epoch_1000x as the x-axis in tensorboard.
             This calibrates different curves when batch size changes.
@@ -175,8 +178,9 @@ def valid_one_epoch(model: torch.nn.Module,
             ssl_masks = data['mask'].to(device, non_blocking=True) # 0 is keep, 1 is remove
             full_samples = data['full'].to(device, non_blocking=True)
 
-            samples = samples*(mask.to(samples.device))
-            ssl_masks = smasks.to(samples.device)
+            if args.downsample < 2:
+                samples = samples*(mask.to(samples.device))
+                ssl_masks = smasks.to(samples.device)
 
             _, _, pred, _  = model(samples, ssl_masks, full_samples)
 

models_mae.py
@@ -264,6 +264,7 @@ class MaskedAutoencoderViT(nn.Module):
             print('anomaly detected d1')
 
         # apply Transformer blocks
+        '''
         for i, blk in enumerate(self.decoder_blocks):
             if i > 5:
                 with torch.cuda.amp.autocast(enabled=False):
@@ -272,7 +273,9 @@ class MaskedAutoencoderViT(nn.Module):
                 x = blk(x)
             if not torch.isfinite(x).all():
                 print('anomaly detected in after {}th block'.format(i))
-
+        '''
+        for blk in self.decoder_blocks:
+            x = blk(x)
         
 
         if not torch.isfinite(x).all():
@@ -297,8 +300,6 @@ class MaskedAutoencoderViT(nn.Module):
         """
         N,L,_=pred.shape
 
-        #sp_masks = 1-ssl_masks
-
         sp_masks = self.patchify(sp_masks)
         if full is not None:
             target = self.patchify(full)
@@ -311,7 +312,8 @@ class MaskedAutoencoderViT(nn.Module):
 
         # loss = (pred - target) ** 2
         loss = torch.abs(pred - target)
-        loss = loss*sp_masks
+        if self.ssl: # calculate loss in only acquired data
+            loss = loss*sp_masks
         if self.divide_loss is not None:
             divide_loss = self.patchify(self.divide_loss)
             loss = loss*divide_loss.to(loss.device)
@@ -319,7 +321,10 @@ class MaskedAutoencoderViT(nn.Module):
 
         # loss = (loss * mask).sum() / N  # mean loss on removed patches
         # loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
-        loss = loss.sum() / N # mean loss on every patches
+        if self.ssl:
+            loss = loss.sum() / mask.sum() # mean loss on every patches
+        else:
+            loss = loss.sum() / N
 
         return loss
 
@@ -341,7 +346,6 @@ class MaskedAutoencoderViT(nn.Module):
 
 
     def forward(self, imgs, ssl_masks, full, mask_ratio=0.75):
-
         latent1, mask1, ids_restore1, pair_ids = self.forward_encoder(imgs, mask_ratio)
         pred1 = self.forward_decoder(latent1, ids_restore1)  # [N, L, p*p*3]
         ppred1 = self.predictor(pred1)
@@ -382,14 +386,28 @@ class MaskedAutoencoderViT(nn.Module):
 
 
 
-def mae_vit_base_patch16_uniform_dec768d12b(**kwargs):
+def mae_2d_large_12_768(**kwargs):
     model = MaskedAutoencoderViT(
         patch_size=16, in_chans=2, embed_dim=768, depth=12, num_heads=12,
         decoder_embed_dim=768, decoder_depth=12, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
+def mae_2d_base_8_768(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, in_chans=2, embed_dim=768, depth=8, num_heads=12,
+        decoder_embed_dim=768, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
 
+def mae_2d_small_4_768(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, in_chans=2, embed_dim=768, depth=4, num_heads=12,
+        decoder_embed_dim=768, decoder_depth=4, decoder_num_heads=12,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+'''
 def mae_vit_base_patch16_dec512d8b(**kwargs):
     model = MaskedAutoencoderViT(
         patch_size=16, embed_dim=768, depth=12, num_heads=12,
@@ -412,10 +430,12 @@ def mae_vit_huge_patch14_dec512d8b(**kwargs):
         decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
-
+'''
 
 # set recommended archs
-mae_vit_base_patch16 = mae_vit_base_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
-mae_vit_large_patch16 = mae_vit_large_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
-mae_vit_huge_patch14 = mae_vit_huge_patch14_dec512d8b  # decoder: 512 dim, 8 blocks
-mae_vit_base_patch16_uniform = mae_vit_base_patch16_uniform_dec768d12b #decoder: 768 dim, 12 blocks
+# mae_vit_base_patch16 = mae_vit_base_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
+# mae_vit_large_patch16 = mae_vit_large_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
+# mae_vit_huge_patch14 = mae_vit_huge_patch14_dec512d8b  # decoder: 512 dim, 8 blocks
+mae2d_large = mae_2d_large_12_768 #decoder: 768 dim, 12 blocks
+mae2d_base = mae_2d_base_8_768
+mae2d_small = mae_2d_small_4_768

models_mae_1d.py
@@ -170,6 +170,8 @@ class MaskedAutoencoderViT1d(nn.Module):
         
         if torch.sum(ssl_masks) != 0:
             removed_index = ssl_masks[0,0,:,0].nonzero(as_tuple=True)[0]
+            if len_keep+len(removed_index)>L:
+                len_keep = L - len(removed_index)
         else:
             removed_index = None
 
@@ -289,6 +291,7 @@ class MaskedAutoencoderViT1d(nn.Module):
             print('anomaly detected d1')
 
         # apply Transformer blocks
+        '''
         for i, blk in enumerate(self.decoder_blocks):
             if i > 5:
                 with torch.cuda.amp.autocast(enabled=False):
@@ -297,7 +300,9 @@ class MaskedAutoencoderViT1d(nn.Module):
                 x = blk(x)
             if not torch.isfinite(x).all():
                 print('anomaly detected in after {}th block'.format(i))
-
+        '''
+        for blk in self.decoder_blocks:
+            x = blk(x)
         
 
         if not torch.isfinite(x).all():
@@ -325,10 +330,10 @@ class MaskedAutoencoderViT1d(nn.Module):
         #sp_masks = 1-ssl_masks
 
         sp_masks = self.patchify(sp_masks)
-        if full is not None:
-            target = self.patchify(full)
-        else:
+        if self.ssl:
             target = self.patchify(imgs)
+        else:
+            target = self.patchify(full)
         if self.norm_pix_loss:
             mean = target.mean(dim=-1, keepdim=True)
             var = target.var(dim=-1, keepdim=True)
@@ -336,7 +341,8 @@ class MaskedAutoencoderViT1d(nn.Module):
 
         # loss = (pred - target) ** 2
         loss = torch.abs(pred - target)
-        loss = loss*sp_masks
+        if self.ssl:
+            loss = loss*sp_masks
         if self.divide_loss is not None:
             divide_loss = self.patchify(self.divide_loss)
             loss = loss*divide_loss.to(loss.device)
@@ -409,14 +415,28 @@ class MaskedAutoencoderViT1d(nn.Module):
 
 
 
-def mae_vit_1d_base_patch16_uniform_dec768d12b(**kwargs):
+def mae_1d_large_12_1024(**kwargs):
     model = MaskedAutoencoderViT1d(
-        patch_size=16, in_chans=2, embed_dim=768, depth=12, num_heads=12,
-        decoder_embed_dim=768, decoder_depth=12, decoder_num_heads=16,
+        patch_size=16, in_chans=2, embed_dim=1024, depth=12, num_heads=12,
+        decoder_embed_dim=1024, decoder_depth=12, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
+def mae_1d_base_8_768(**kwargs):
+    model = MaskedAutoencoderViT1d(
+        patch_size=16, in_chans=2, embed_dim=768, depth=8, num_heads=12,
+        decoder_embed_dim=768, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
 
+def mae_1d_small_4_768(**kwargs):
+    model = MaskedAutoencoderViT1d(
+        patch_size=16, in_chans=2, embed_dim=768, depth=4, num_heads=12,
+        decoder_embed_dim=768, decoder_depth=4, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+'''
 def mae_vit_1d_base_patch16_dec512d8b(**kwargs):
     model = MaskedAutoencoderViT1d(
         patch_size=16, embed_dim=768, depth=12, num_heads=12,
@@ -439,10 +459,15 @@ def mae_vit_1d_huge_patch14_dec512d8b(**kwargs):
         decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
-
+'''
 
 # set recommended archs
+'''
 mae_vit_1d_base_patch16 = mae_vit_1d_base_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
 mae_vit_1d_large_patch16 = mae_vit_1d_large_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
 mae_vit_1d_huge_patch14 = mae_vit_1d_huge_patch14_dec512d8b  # decoder: 512 dim, 8 blocks
 mae_vit_1d_base_patch16_uniform = mae_vit_1d_base_patch16_uniform_dec768d12b #decoder: 768 dim, 12 blocks
+'''
+mae1d_large = mae_1d_large_12_1024
+mae1d_base = mae_1d_base_8_768
+mae1d_small =  mae_1d_small_4_768

test.py
@@ -15,38 +15,23 @@ from torch.utils.tensorboard import SummaryWriter
 
 import models_mae_1d
 import models_mae
-import util.misc as misc
+# import util.misc as misc
 
 def get_args_parser():
     parser = argparse.ArgumentParser('MAE test', add_help=False)
     # Model parameters
-    parser.add_argument('--model', default='mae_vit_base_patch16_uniform', type=str, metavar='MODEL',
-                        help='Name of model to train')
-                        #mae_vit_base_patch16
-                        #mae_vit_large_patch16
-                        #mae_vit_huge_patch14
-                        #mae_vit_base_patch16_uniform
-
+    parser.add_argument('--model', default='mae2d_large', type=str, choices=['mae2d_large', 'mae2d_base', 'mae2d_small', 'mae1d_base'],
+                        metavar='MODEL', help='Name of model to train')
     parser.add_argument('--input_size', default=256, type=int, #default 224
                         help='images input size')
-
-    parser.add_argument('--mask_ratio', default=0.5, type=float,
-                        help='Masking ratio (percentage of removed patches).')
-    parser.add_argument('--norm_pix_loss', action='store_true',
-                        help='Use (per-patch) normalized pixels as targets for computing loss')
-    parser.set_defaults(norm_pix_loss=False)
     parser.add_argument('--ssl', action='store_true',
                         help='make two different augmentation for each data, and calculate self supervised loss')
-    parser.add_argument('--ssl_weight', type=float, default=1, help='weight of ssl loss related to sp_loss')
-    
-
 
     # Data Preprocessing
     parser.add_argument('--down', default='uniform', choices=['uniform', 'random'], 
                         help='method of constructing undersampled data')
-    parser.add_argument('--downsample', type=int, default=2, help='downsampling factor of original data')
+    parser.add_argument('--downsample', type=int, default=4, help='downsampling factor of original data')
     parser.add_argument('--low_freq_ratio', type=float, default=0.7, help='ratio of low frequency lines in undersampled data')
-    parser.add_argument('--no_center_mask', action='store_true', help='preserving center in kspace from random_masking')
 
     # Dataset parameters
     parser.add_argument('--data_path', default='../../data/', type=str,
@@ -63,7 +48,7 @@ def get_args_parser():
     parser.add_argument('--seed', default=0, type=int)
     parser.add_argument('--resume', default='',
                         help='resume from checkpoint. ex)1023_mae/checkpoint-best.pth')
-    parser.add_argument('--save_num', default=0, type=int, help='0 is saving all images, otherwise saving only that number of images')
+    parser.add_argument('--save_num', default=100, type=int, help='0 is saving all images, otherwise saving only that number of images')
 
     # distributed training parameters
     parser.add_argument('--world_size', default=1, type=int,
@@ -75,7 +60,7 @@ def get_args_parser():
 
 
 def main(args):
-    misc.init_distributed_mode(args)
+    # misc.init_distributed_mode(args)
 
     args.resume = os.path.join(args.output_dir, args.resume)
     args.output_dir = '/'.join(args.resume.split('/')[:-1])
@@ -85,30 +70,29 @@ def main(args):
     device = torch.device(args.device)
 
     # fix the seed for reproducibility
-    seed = args.seed + misc.get_rank()
+    # seed = args.seed + misc.get_rank()
+    seed = args.seed
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     np.random.seed(seed)
 
     dataset = IXIDataset(args, mode='test')
-    global_rank = misc.get_rank()
+    # global_rank = misc.get_rank()
 
-    if global_rank == 0 and args.log_dir is not None:
-        os.makedirs(args.log_dir, exist_ok=True)
-        log_writer = SummaryWriter(log_dir=args.log_dir)
-    else:
-        log_writer = None
+    # if global_rank == 0 and args.log_dir is not None:
+    #     os.makedirs(args.log_dir, exist_ok=True)
+    #     log_writer = SummaryWriter(log_dir=args.log_dir)
+    # else:
+    #     log_writer = None
 
     data_loader = torch.utils.data.DataLoader(
         dataset, batch_size=1, num_workers=10, pin_memory=True, drop_last=False
     )
 
     if '1d' not in args.model:
-        model = models_mae.__dict__[args.model](norm_pix_loss=args.norm_pix_loss, ssl=args.ssl, 
-                                                no_center_mask=args.no_center_mask, num_low_freqs = dataset.num_low_freqs)
+        model = models_mae.__dict__[args.model](ssl=args.ssl)
     else:
-        model = models_mae_1d.__dict__[args.model](norm_pix_loss=args.norm_pix_loss, ssl=args.ssl, 
-                                                no_center_mask=args.no_center_mask, num_low_freqs = dataset.num_low_freqs)
+        model = models_mae_1d.__dict__[args.model](ssl=args.ssl)
 
     model.to(device)
 
@@ -144,7 +128,7 @@ def main(args):
             samples = data['down'].to(device, non_blocking=True)
             ssl_masks = data['mask'].to(device, non_blocking=True) # 0 is keep, 1 is remove
             full_samples = data['full'].to(device, non_blocking=True)
-            _, _, pred, _  = model(samples, ssl_masks)
+            _, _, pred, _  = model(samples, ssl_masks, full_samples)
 
 
             # Data consistency
@@ -178,6 +162,8 @@ def main(args):
             if args.save_num==i-1:
                 break
 
+    with open(os.path.join(save_folder, 'test_log.txt'), mode='a', encoding='uft-8') as f:
+        f.write(json.dumps(test_stats)+'\n')
     print('Test: {}'.format(', '.join(['{}: {:.3f}'.format(k,v.item()) for k,v in test_stats.items()])))
 
 