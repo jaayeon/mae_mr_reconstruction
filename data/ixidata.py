fix validation masking and spatial loss calculation

data/ixidata.py
@@ -30,15 +30,18 @@ class IXIDataset(Dataset):
 
         #downsample
         self.down = opt.down
-        self.do_downsample = True if opt.downsample>1 else False
-
+    
         self.rng = np.random.RandomState(opt.seed)
-
-        self.datalist = glob.glob(os.path.join(opt.data_path,opt.dataset,mode,'*','*','*.pkl'))
+        self.datalist = sorted(glob.glob(os.path.join(opt.data_path,opt.dataset,mode,'*','*','*.pkl')))
         
-        self.num_low_freqs = int(opt.input_size/opt.downsample*opt.low_freq_ratio) #select 70% from center line
-        self.num_high_freqs = int(opt.input_size/opt.downsample)-self.num_low_freqs
-
+        if mode=='valid':
+            self.do_downsample = True if opt.v_downsample>1 else False
+            self.num_low_freqs = int(opt.input_size/opt.v_downsample*opt.low_freq_ratio) #select 70% from center line
+            self.num_high_freqs = int(opt.input_size/opt.v_downsample)-self.num_low_freqs
+        else:
+            self.do_downsample = True if opt.downsample>1 else False
+            self.num_low_freqs = int(opt.input_size/opt.downsample*opt.low_freq_ratio) #select 70% from center line
+            self.num_high_freqs = int(opt.input_size/opt.downsample)-self.num_low_freqs
 
     def __getitem__(self, idx):
         with open(self.datalist[idx], 'rb') as f:

engine_pretrain.py
@@ -64,7 +64,7 @@ def train_one_epoch(model: torch.nn.Module,
             pred_dc = samples + pred*ssl_masks
         else:
             mask = mask.unsqueeze(1).unsqueeze(-1).repeat(1,2,1,256)
-            pred_dc = samples + pred*mask
+            pred_dc = samples*(1-mask) + pred*mask # 0 is keep, 1 is remove
 
         pred_dc, full = rifft2(pred_dc[:,:,:,:], full_samples[:,:,:,:], permute=True) 
         maxnum = torch.max(full)
@@ -143,7 +143,7 @@ def valid_one_epoch(model: torch.nn.Module,
 
     if not os.path.exists(save_folder):
         os.mkdir(save_folder)
-
+    """ 
     num_low_freqs = 44
     num_high_freqs = 20
     h=256
@@ -157,9 +157,9 @@ def valid_one_epoch(model: torch.nn.Module,
     accel_mask = np.zeros(h, dtype=np.float32)
     accel_mask[0::adjusted_accel]=1
     accel_mask = torch.tensor(accel_mask).view(1,-1,1)
-    mask = torch.max(center_mask, accel_mask)
+    mask = torch.max(center_mask, accel_mask) # 1 is keep, 0 is remove
     smasks = torch.ones(2,256,256)*(1-mask)
-
+    """
 
     with torch.no_grad():
         for i, data in enumerate(data_loader):
@@ -167,11 +167,11 @@ def valid_one_epoch(model: torch.nn.Module,
             ssl_masks = data['mask'].to(device, non_blocking=True) # 0 is keep, 1 is remove
             full_samples = data['full'].to(device, non_blocking=True)
 
-            if args.downsample < 2:
+            """ if args.downsample < 2:
                 samples = samples*(mask.to(samples.device))
-                ssl_masks = smasks.to(samples.device)
+                ssl_masks = smasks.to(samples.device) """
 
-            _, _, pred, _  = model(samples, ssl_masks, full_samples)
+            _, _, pred, _  = model(samples, ssl_masks, full_samples, mask_ratio=0)
 
             # Data consistency
             pred = torch.clamp(pred, min=-1, max=1)

main_pretrain.py
@@ -91,6 +91,7 @@ def get_args_parser():
     parser.add_argument('--down', default='uniform', choices=['uniform', 'random'], 
                         help='method of constructing undersampled data')
     parser.add_argument('--downsample', type=int, default=4, help='downsampling factor of original data')
+    parser.add_argument('--v_downsample', type=int, default=4, help='downsampling factor of validation data')
     parser.add_argument('--low_freq_ratio', type=float, default=0.7, help='ratio of low frequency lines in undersampled data')
     parser.add_argument('--no_center_mask', action='store_true', help='preserving center in kspace from random_masking')
 
@@ -167,12 +168,9 @@ def main(args):
     cudnn.benchmark = True
 
     # dataset
-    dataset = IXIDataset(args, mode='train')
-
-    tnum = int(len(dataset)*0.95)
-    vnum = len(dataset)-tnum
-    dataset_train, dataset_valid = torch.utils.data.random_split(dataset, [tnum, vnum])
-
+    dataset_train = IXIDataset(args, mode='train')
+    dataset_valid = IXIDataset(args, mode='valid')
+    print('dataset_train numlow: {}, valid numlow: {}'.format(dataset_train.num_low_freqs, dataset_valid.num_low_freqs))
 
     global_rank = misc.get_rank()
     if args.distributed:
@@ -205,7 +203,7 @@ def main(args):
         pin_memory=args.pin_mem, 
         drop_last=False
     )
-    num_low_freqs = 44 if dataset.num_low_freqs>44 else dataset.num_low_freqs
+    num_low_freqs = 44 if dataset_valid.num_low_freqs>44 else dataset_valid.num_low_freqs
     # define the model
     if '1d' not in args.model:
         model = models_mae.__dict__[args.model](norm_pix_loss=args.norm_pix_loss, ssl=args.ssl, 

models_mae_1d.py
@@ -258,7 +258,7 @@ class MaskedAutoencoderViT1d(nn.Module):
         x = x + self.pos_embed[:, 1:, :]
 
         # masking: length -> length * mask_ratio
-        if self.train and self.mae:
+        if self.mae:
             x, mask, ids_restore, pair_ids = self.random_masking(x, mask_ratio, ssl_masks, given_ids_shuffle=given_ids_shuffle)
         else:
             mask = None
@@ -283,7 +283,7 @@ class MaskedAutoencoderViT1d(nn.Module):
         # embed tokens
         x = self.decoder_embed(x)
 
-        if self.train and self.mae:
+        if self.mae:
             # append mask tokens to sequence
             mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1) #(1,1,D)->(N,L*0.75,D)
             x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token