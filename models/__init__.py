34145288d45a73bf248589740c035b3f7130f230
jaayeon <jaayeon5200@gmial.com>
2023-03-21 21:30:30 +0900

    fix init

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .models_mae import *
 from .models_mae_1d import *
 from .models_vit import *
-from .models_himae import *
+# from .models_himae import *
 from .alternative_mae import *
 from .models_mtrans import *
