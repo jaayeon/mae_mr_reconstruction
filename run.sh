0f0c9fb12f9f90cc6749e48aaceb43d206562809
jaayeon <jaayeon5200@gmial.com>
2023-04-04 11:05:37 +0900

    add sleep

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-python main_pretrain.py --model vit1d_large --dataset fastmri --input_size 320 --patch_direction pe --batch_size 32
-
-python main_pretrain.py --model vit2d_large --dataset fastmri --input_size 320 --batch_size 16
+python sleep.py
+python main_pretrain.py --model vit2d_large --dataset fastmri --input_size 320 --batch_size 16 --epochs 100
+python main_pretrain.py --model mae2d_large --dataset fastmri --input_size 320 --batch_size 16 --epochs 100
---
+++ sleep.py
@@ -0,0 +1,5 @@
+import time
+
+print('wait 2 hours')
+time.sleep(60*60*2)
+print('start the code!')
