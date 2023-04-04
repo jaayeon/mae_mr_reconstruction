35c0b85c30b40efe6202b2651bc78081379c17bd
305sv2021-1 <jaayeon5200@gmail.com>
2023-04-04 14:53:26 +0900

    ignore sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python sleep.py
-python main_pretrain.py --model vit2d_large --dataset fastmri --input_size 320 --batch_size 16 --epochs 100
-python main_pretrain.py --model mae2d_large --dataset fastmri --input_size 320 --batch_size 16 --epochs 100
+python main_pretrain.py --model vit1d_large --dataset fastmri --patch_direction pe --input_size 320 --batch_size 16 --epochs 100
+python main_pretrain.py --model mae1d_large --dataset fastmri --patch_direction pe --input_size 320 --batch_size 16 --epochs 100
